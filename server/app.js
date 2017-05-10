fe27b7936928adf1ed5de9d094eb058625d03f6b
Chris Weed <chris@cjweed.com>
2017-05-09 20:48:31 -0500

    Fixed the weather fetch

--- server/app.js
+++ server/app.js
@@ -6,11 +6,12 @@ app.use(express.static('public'))
 const port = process.env.PORT || 8080
 console.log('Using city: ' + process.env.CITY_ID)
 
-let weatherCache = {"coord":{"lon":-89.33,"lat":42.01},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":58.44,"pressure":1014,"humidity":71,"temp_min":55.4,"temp_max":60.8},"visibility":16093,"wind":{"speed":3.36,"deg":180},"clouds":{"all":90},"dt":1494362100,"sys":{"type":1,"id":974,"message":0.0288,"country":"US","sunrise":1494326518,"sunset":1494378366},"id":4904898,"name":"Oregon","cod":200}
+let weatherCache = {}
 let lastWeatherUpdate = 0
 
 app.get('/weather', (req, res) => {
-  if (!lastWeatherUpdate || lastWeatherUpdate - new Date().getTime() >= 900000) {
+  if (new Date().getTime() - lastWeatherUpdate >= 900000) {
+    console.log('Refreshing Weather Data!')
     request.get(`http://api.openweathermap.org/data/2.5/weather?id=${process.env.CITY_ID}&APPID=${process.env.OPEN_WEATHER_API}&units=imperial`, (err, resp, body) => {
       if (err) {
         console.error(err)
