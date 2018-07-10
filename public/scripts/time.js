38db0f41f893bc8e29ba221e25046cffafffb1ec
Chris Weed <chris@cjweed.com>
2018-07-10 04:36:14 -0500

    Time greys but is visible overnight.  Request full screen is on click of time.

--- public/scripts/time.js
+++ public/scripts/time.js
@@ -6,5 +6,15 @@ var time = {
   update: function() {
     document.getElementsByClassName('date')[0].innerHTML = moment().format('dddd, MMMM Do')
     document.getElementsByClassName('time')[0].innerHTML = moment().format('h:mm')
+
+    var now = new Date()
+    if (now.getHours() >= this.start && now.getHours() < this.end) {
+      document.getElementsByClassName('date')[0].style.color = 'grey'
+      document.getElementsByClassName('time')[0].style.color = 'grey'
+    }
+    else {
+      document.getElementsByClassName('date')[0].style.color = 'white'
+      document.getElementsByClassName('time')[0].style.color = 'white'
+    }
   }
 }
