65cf6cd6704cee4e66a7fb725ca1bbc0b1116e7d
Chris Weed <chris@cjweed.com>
2018-07-13 22:05:41 -0500

    Removed the awesome blur since it was crashing an Android device

--- public/scripts/primephotos.js
+++ public/scripts/primephotos.js
@@ -38,14 +38,14 @@ var prime = {
       // TODO make this work better, but I'm watching football so it's good enough
       if (this.currentFrameIndex === 0) {
         this.frames[0].style.opacity = 0
-        this.blurFrames[0].style.opacity = 0
+        // this.blurFrames[0].style.opacity = 0
         setTimeout(function() {
           // Load the next image into this frame
           me.setImage(0)
         }, 1200)
       } else {
         this.frames[0].style.opacity = 1
-        this.blurFrames[0].style.opacity = 1
+        // this.blurFrames[0].style.opacity = 1
         setTimeout(function() {
           // Load the image in the other frame (the one now hidden under this)
           me.setImage(1)
@@ -56,6 +56,6 @@ var prime = {
   setImage: function(frameIndex) {
     var rando = Math.random()
     this.frames[frameIndex].setAttribute('style', 'background-image: url("/image?r=' + rando + '")')
-    this.blurFrames[frameIndex].setAttribute('style', 'background-image: url("/image?r=' + rando + '")')
+    // this.blurFrames[frameIndex].setAttribute('style', 'background-image: url("/image?r=' + rando + '")')
   }
 }
